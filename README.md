0fdd33f6fb4229f6988eb8ad71667962310f9640
stas <stas@vasin.name>
2023-08-03 19:56:45 +0200

    Local Status Boost Bot

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# local_status_boost
+# Local Status Boost
+
+Local Status Boost - это программа, которая автоматически репостит посты в локальной ленте Mastodon, игнорируя определенный аккаунт.
+
+### Требования
+
+- Rust 1.71.0 (возможны и более старые версии, но не тестировал)
+- Доступ к сети Mastodon и соответствующий access token
+
+### Установка
+
+1. Клонируйте репозиторий
+
+```
+git clone https://github.com/yourusername/local-status-boost.git
+```
+
+2. Запустите Cargo для сборки проекта
+
+```
+cargo build --release
+```
+
+### Настройка
+
+Программа использует файл `Config.toml` для своих настроек. Он должен быть настроен следующим образом:
+
+```toml
+api_host = "https://your-instance-here"
+access_token = "your-access-token-here"
+filter_account = "@account-to-ignore"
+```
+
+Замените `https://your-instance-here` на URL вашего сервера Mastodon, `your-access-token-here` на ваш access token, а `@account-to-ignore` на аккаунт, который вы хотите игнорировать (рекомендуется использовать @local и он естественно должен совпадать с тем от имени которого вы будете репостить посты).
+
+### Запуск
+
+Просто запустите скомпилированную программу:
+
+```
+./target/release/local-status-boost
+```
+
+Программа будет автоматически репостить все посты в локальной ленте, за исключением постов от аккаунта, указанного в `filter_account`.
+
+### Завершение работы
+
+Чтобы прервать программу, просто нажмите `Ctrl+C`.# local_status_boost
---
+++ src/main.rs
@@ -0,0 +1,98 @@
+use megalodon::{entities, error, generator, SNS};
+use std::fs::File;
+use std::io::Read;
+use env_logger;
+use tokio::time::{sleep, Duration};
+use tokio::signal::ctrl_c;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+    api_host: String,
+    access_token: String,
+    filter_account: String,
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let ctrl_c = ctrl_c();
+
+    tokio::select! {
+        _ = async_main() => {},
+        _ = ctrl_c => {
+            println!("Received Ctrl+C. Shutting down.");
+        },
+    }
+}
+
+async fn async_main() {
+    let mut file = File::open("Config.toml").expect("Unable to open the file");
+    let mut config_string = String::new();
+    file.read_to_string(&mut config_string).expect("Unable to read the file");
+
+    let config: Config = match toml::from_str(&config_string) {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Error parsing Config.toml: {}", e);
+            return;
+        }
+    };
+
+    match verify_credentials(&config.api_host, &config.access_token).await {
+        Ok(_account) => {
+            let client = generator(megalodon::SNS::Mastodon, config.api_host.clone(), Some(config.access_token), None);
+            loop {
+                match get_local_status(&client).await {
+                    Ok(mut statuses) => {
+                        statuses.reverse(); // Reverse the order of statuses
+                        for status in statuses.iter().filter(|s| s.account.username != config.filter_account) {
+                            println!("Received message: {}", status.id);
+                            match boost_status(&client, &status.id).await {
+                                Ok(res) => {
+                                    println!("Boosted: {}", res.id);
+                                },
+                                Err(err) => {
+                                    println!("Error boosting {}: {}", status.id, err);
+                                }
+                            }
+                        }
+                    },
+                    Err(err) => {
+                        println!("Error getting local status: {}", err);
+                    }
+                }
+                sleep(Duration::from_secs(30)).await;
+            }
+        }
+        Err(e) => {
+            println!("Error: Could not verify credentials - {}", e);
+        }
+    }
+}
+
+
+async fn verify_credentials(
+    url: &str,
+    access_token: &str,
+) -> Result<entities::Account, error::Error> {
+    let client = generator(SNS::Mastodon, url.to_string(), Some(access_token.to_string()), None);
+    let res = client.verify_account_credentials().await?;
+    Ok(res.json())
+}
+
+async fn get_local_status(
+    client: &Box<dyn megalodon::Megalodon + Send + Sync>,
+) -> Result<Vec<entities::Status>, error::Error> {
+    let res = client.get_local_timeline(None).await.map_err(|e| error::Error::from(e))?;
+    Ok(res.json())
+}
+
+async fn boost_status(
+    client: &Box<dyn megalodon::Megalodon + Send + Sync>,
+    id: &str,
+) -> Result<entities::Status, error::Error> {
+    let res = client.reblog_status(id.to_string()).await.map_err(|e| error::Error::from(e))?;
+    Ok(res.json())
+}
